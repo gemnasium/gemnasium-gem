5549f5ded03fc0561877bab2d339efffb15347a0
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2013-03-05 15:26:54 +0100

    Create tmp folder for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,6 @@ require 'rspec'
 require 'webmock/rspec'
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+FileUtils.mkdir_p 'tmp'
 
 WebMock.disable_net_connect!
